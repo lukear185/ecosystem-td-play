08427dec2b5c639af151e14b462958032d5e565d
lukear185 <gaisesiru1111@gmail.com>
2026-08-31 12:49:15 +0900

    Animate castle clockwork

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788146162|2492328';
+const CACHE_VERSION = '1788147936|2784518';
 /** @type {string} */
 const CACHE_PREFIX = '人工太陽炉 外郭路-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
