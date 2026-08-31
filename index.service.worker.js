f330cfdaafe528c92ed1bc7b4220dc647cc9d47d
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 01:07:54 +0900

    Fix all iPhone battle controls

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788190460|3521602';
+const CACHE_VERSION = '1788192159|3553971';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
