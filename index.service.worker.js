e99ab780d5013fd48914a095cc9bc35648a251d8
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 00:41:39 +0900

    Fix iPhone start button input

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788188165|2638628';
+const CACHE_VERSION = '1788190460|3521602';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
