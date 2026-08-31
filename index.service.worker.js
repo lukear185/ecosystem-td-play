128a3e50a3c43dad7ffca4ccc84ee3c0f3b6827f
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 07:33:07 +0900

    deploy: publish full visual redesign

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788210676|2521960';
+const CACHE_VERSION = '1788215325|3113258';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-v3-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
