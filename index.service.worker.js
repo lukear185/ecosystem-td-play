efcc5d419984343f01dea1ef4ca5f5c76ba67acf
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 04:24:14 +0900

    Deploy authored art direction rebuild

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788198000|2615839';
+const CACHE_VERSION = '1788203922|2652868';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-v3-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
