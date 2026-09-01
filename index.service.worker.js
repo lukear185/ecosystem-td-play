d842d06aa82c898184226f6d44f5c78dcf433b0c
lukear185 <gaisesiru1111@gmail.com>
2026-09-02 02:05:14 +0900

    Publish the first two campaign areas

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788272357|3289505';
+const CACHE_VERSION = '1788281866|3204198';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-v3-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
