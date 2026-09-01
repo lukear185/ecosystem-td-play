c9d05227ae98e0f0eb70fe8ef03fdbf184e2b9da
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 13:02:43 +0900

    Deploy social home and tap entry flow

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788230858|3058695';
+const CACHE_VERSION = '1788234953|3336817';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-v3-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
