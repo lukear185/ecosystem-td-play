db281381294c29519d45fef3c56dc5c4798a012c
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 23:33:20 +0900

    Run game presentation at 60 fps

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788270829|3316967';
+const CACHE_VERSION = '1788272357|3289505';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-v3-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
