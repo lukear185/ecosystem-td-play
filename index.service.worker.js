221693ac850ff5e339bee222a502ab5fd270fcee
lukear185 <gaisesiru1111@gmail.com>
2026-09-01 15:35:10 +0900

    Deploy mobile presentation and thermal improvements

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788234953|3336817';
+const CACHE_VERSION = '1788244065|3427891';
 /** @type {string} */
 const CACHE_PREFIX = 'ecosystem-td-play-v3-sw-cache-';
 const LEGACY_CACHE_PREFIXES = [
